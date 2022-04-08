51e53dafef0860d4efe2c4d5df149ddb080ab846
ice1000 <ice1000kotlin@foxmail.com>
2022-04-08 04:36:56 -0400

    MISC: tweak code style

--- guest0x0-base/src/main/java/org/aya/guest0x0/syntax/Expr.java
+++ guest0x0-base/src/main/java/org/aya/guest0x0/syntax/Expr.java
@@ -7,18 +7,12 @@ import org.jetbrains.annotations.NotNull;
 
 public sealed interface Expr {
   @NotNull SourcePos pos();
-  record Unresolved(@Override @NotNull SourcePos pos, String name) implements Expr {
-  }
-
-  record Resolved(@Override @NotNull SourcePos pos, LocalVar ref) implements Expr {
-  }
+  record Unresolved(@Override @NotNull SourcePos pos, String name) implements Expr {}
+  record Resolved(@Override @NotNull SourcePos pos, LocalVar ref) implements Expr {}
 
   /** @param isApp it's a tuple if false */
-  record Two(boolean isApp, @Override @NotNull SourcePos pos, Expr f, Expr a) implements Expr {
-  }
-
-  record Lam(@Override @NotNull SourcePos pos, LocalVar x, Expr a) implements Expr {
-  }
+  record Two(boolean isApp, @Override @NotNull SourcePos pos, Expr f, Expr a) implements Expr {}
+  record Lam(@Override @NotNull SourcePos pos, LocalVar x, Expr a) implements Expr {}
 
   static @NotNull Expr unlam(@NotNull DynamicSeq<LocalVar> binds, int n, @NotNull Expr body) {
     if (n == 0) return body;
@@ -29,17 +23,12 @@ public sealed interface Expr {
   }
 
   /** @param isOne it's a second projection if false */
-  record Proj(@Override @NotNull SourcePos pos, @NotNull Expr t, boolean isOne) implements Expr {
-  }
+  record Proj(@Override @NotNull SourcePos pos, @NotNull Expr t, boolean isOne) implements Expr {}
 
   /** @param isU it's the interval type if false */
-  record UI(@Override @NotNull SourcePos pos, boolean isU) implements Expr {
-  }
+  record UI(@Override @NotNull SourcePos pos, boolean isU) implements Expr {}
 
   /** @param isPi it's a sigma if false */
-  record DT(boolean isPi, @Override @NotNull SourcePos pos, Param<Expr> param, Expr cod) implements Expr {
-  }
-
-  record Path(@Override @NotNull SourcePos pos, @NotNull Boundary.Data<Expr> data) implements Expr {
-  }
+  record DT(boolean isPi, @Override @NotNull SourcePos pos, Param<Expr> param, Expr cod) implements Expr {}
+  record Path(@Override @NotNull SourcePos pos, @NotNull Boundary.Data<Expr> data) implements Expr {}
 }
--- guest0x0-base/src/main/java/org/aya/guest0x0/syntax/Term.java
+++ guest0x0-base/src/main/java/org/aya/guest0x0/syntax/Term.java
@@ -10,20 +10,11 @@ public sealed interface Term {
     return new Normalizer(MutableMap.create(), MutableMap.of(x, t)).term(this);
   }
 
-  record Ref(@NotNull LocalVar var) implements Term {
-  }
-
-  record Call(@NotNull LocalVar fn, @NotNull ImmutableSeq<Term> args) implements Term {
-  }
-
-  record Two(boolean isApp, @NotNull Term f, @NotNull Term a) implements Term {
-  }
-
-  record Proj(@NotNull Term t, boolean isOne) implements Term {
-  }
-
-  record Lam(@NotNull Param<Term> param, @NotNull Term body) implements Term {
-  }
+  record Ref(@NotNull LocalVar var) implements Term {}
+  record Call(@NotNull LocalVar fn, @NotNull ImmutableSeq<Term> args) implements Term {}
+  record Two(boolean isApp, @NotNull Term f, @NotNull Term a) implements Term {}
+  record Proj(@NotNull Term t, boolean isOne) implements Term {}
+  record Lam(@NotNull Param<Term> param, @NotNull Term body) implements Term {}
 
   static @NotNull Term mkLam(@NotNull ImmutableSeq<Param<Term>> telescope, @NotNull Term body) {
     return telescope.view().foldRight(body, Lam::new);
@@ -38,18 +29,9 @@ public sealed interface Term {
   static @NotNull Term mkPi(@NotNull ImmutableSeq<Param<Term>> telescope, @NotNull Term body) {
     return telescope.view().foldRight(body, (param, term) -> new DT(true, param, term));
   }
-
   @NotNull Term U = new UI(true), I = new UI(false);
-
-  record UI(boolean isU) implements Term {
-  }
-
-  record Path(@NotNull Boundary.Data<Term> data) implements Term {
-  }
-
-  record End(boolean isLeft) implements Term {
-  }
-
-  record PLam(@NotNull ImmutableSeq<LocalVar> dims, @NotNull Term core) implements Term {
-  }
+  record UI(boolean isU) implements Term {}
+  record Path(@NotNull Boundary.Data<Term> data) implements Term {}
+  record End(boolean isLeft) implements Term {}
+  record PLam(@NotNull ImmutableSeq<LocalVar> dims, @NotNull Term core) implements Term {}
 }
