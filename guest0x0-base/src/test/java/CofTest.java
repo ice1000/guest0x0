70b0af7f77c6ed4a9b7bc93e8b2ea8c4211ef4e3
ice1000 <ice1000kotlin@foxmail.com>
2022-05-06 01:10:36 -0400

    TEST: add a simple cof test case, fix bug in tester

--- guest0x0-base/src/test/java/CofTest.java
+++ guest0x0-base/src/test/java/CofTest.java
@@ -10,9 +10,13 @@ import org.aya.guest0x0.tyck.Elaborator;
 import org.aya.guest0x0.tyck.Normalizer;
 import org.aya.guest0x0.tyck.Resolver;
 import org.aya.guest0x0.util.LocalVar;
+import org.aya.pretty.doc.Docile;
 import org.aya.util.error.SourceFile;
 import org.intellij.lang.annotations.Language;
 import org.jetbrains.annotations.NotNull;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class CofTest {
   public Restr<Term> substCof(@Language("TEXT") String s, String i, @Language("TEXT") String to, String... vars) {
@@ -20,7 +24,7 @@ public class CofTest {
     var parser = new Parser(SourceFile.NONE);
     var raw = parser.restr(CliMain.parser(s).psi()).fmap(context._1::expr);
     var cof = raw.mapCond(c -> new Restr.Cond<>(context._2.inherit(c.inst(), Term.I), c.isLeft()));
-    var tot = context._2.inherit(context._1.expr(parser.expr(CliMain.parser(s).expr())), Term.I);
+    var tot = context._2.inherit(context._1.expr(parser.expr(CliMain.parser(to).expr())), Term.I);
     var subst = new Normalizer(context._2.sigma(), MutableMap.of(context._1.env().get(i), tot));
     return subst.restr(cof);
   }
@@ -32,5 +36,12 @@ public class CofTest {
     return Tuple.of(resolver, akJr);
   }
 
+  private static void assertDoc(@Language("TEXT") String expected, Docile actual) {
+    assertEquals(expected, actual.toDoc().renderWithPageWidth(80, false));
+  }
 
+  @Test public void simpleSubst() {
+    var cof = substCof("i = 0 \\/ j = 1", "i", "k", "i", "j", "k");
+    assertDoc("k = 0 \\/ j = 1", cof);
+  }
 }
