08cdc48d546bd54feadfc7e19c1aef473509b4e7
ice1000 <ice1000kotlin@foxmail.com>
2022-06-08 16:59:28 -0400

    CUBE: two parallelograms

--- cube-visualizer/src/main/java/org/aya/cube/visualizer/GuiMain.java
+++ cube-visualizer/src/main/java/org/aya/cube/visualizer/GuiMain.java
@@ -1,7 +1,9 @@
 package org.aya.cube.visualizer;
 
 import org.ice1000.jimgui.JImGui;
+import org.ice1000.jimgui.NativeBool;
 import org.ice1000.jimgui.NativeFloat;
+import org.ice1000.jimgui.util.JImGuiUtil;
 import org.ice1000.jimgui.util.JniLoader;
 import org.jetbrains.annotations.NotNull;
 
@@ -10,21 +12,31 @@ public final class GuiMain implements AutoCloseable {
   private final JImGui window;
   private float offsetX;
   private float offsetY;
-  private @NotNull NativeFloat width = new NativeFloat();
-  private boolean[] faces = new boolean[Face3D.values().length];
+  private final @NotNull NativeFloat cubeLen = new NativeFloat();
+  private NativeBool[] faces = new NativeBool[Face3D.values().length];
 
   public enum Face3D {
     Top, Bottom, Front, Back, Left, Right;
   }
 
-  public GuiMain(JImGui window) {this.window = window;}
+  public GuiMain(JImGui window) {
+    this.window = window;
+    cubeLen.modifyValue(50);
+    for (var face : Face3D.values()) {
+      //noinspection resource
+      faces[face.ordinal()] = new NativeBool();
+    }
+  }
 
   @Override public void close() {
     window.close();
+    cubeLen.close();
+    for (var face : faces) face.close();
   }
 
   public static void main(String... args) {
     JniLoader.load();
+    JImGuiUtil.cacheStringToBytes();
     try (var ui = new GuiMain(new JImGui("Cube visualizer"))) {
       ui.mainLoop();
     }
@@ -37,17 +49,30 @@ public final class GuiMain implements AutoCloseable {
         previewWindow();
         window.end();
       }
+      if (window.begin("Main Control")) {
+        mainControlWindow();
+        window.end();
+      }
       window.render();
+    }
+  }
+
+  private void mainControlWindow() {
+    window.sliderFloat("Width", cubeLen, 5, 200);
+    for (var face : Face3D.values()) {
+      window.toggleButton(face.name(), faces[face.ordinal()]);
     }
   }
 
   private void previewWindow() {
     var x = window.getWindowPosX();
     var y = window.getWindowPosY();
-    offsetX = x + 50;
-    offsetY = y + 80;
-    var userWidth = width.accessValue();
-    hParallelogram(userWidth / 2, userWidth);
+    var userSize = cubeLen.accessValue();
+    offsetX = x + 30;
+    offsetY = y + 50;
+    hParallelogram(userSize / 2, userSize);
+    offsetY += userSize;
+    hParallelogram(userSize / 2, userSize);
   }
 
   private void hParallelogram(float height, float width) {
