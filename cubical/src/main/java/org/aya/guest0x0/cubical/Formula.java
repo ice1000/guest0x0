044e7c8e39d61c96ecc2618467a3343de2278feb
ice1000 <ice1000kotlin@foxmail.com>
2022-08-24 17:28:15 -0400

    CUBICAL: weaker fmap

--- cubical/src/main/java/org/aya/guest0x0/cubical/Formula.java
+++ cubical/src/main/java/org/aya/guest0x0/cubical/Formula.java
@@ -5,15 +5,15 @@ import org.jetbrains.annotations.NotNull;
 import java.util.function.Function;
 
 public sealed interface Formula<E> {
-  @NotNull Formula<E> fmap(@NotNull Function<E, E> f);
+  <T> @NotNull Formula<T> fmap(@NotNull Function<E, T> f);
   /** @param isAnd it's or if false */
   record Conn<E>(boolean isAnd, @NotNull E l, @NotNull E r) implements Formula<E> {
-    public @NotNull Conn<E> fmap(@NotNull Function<E, E> f) {return new Conn<>(isAnd, f.apply(l), f.apply(r));}
+    public <T> @NotNull Conn<T> fmap(@NotNull Function<E, T> f) {return new Conn<>(isAnd, f.apply(l), f.apply(r));}
   }
   record Inv<E>(@NotNull E i) implements Formula<E> {
-    public @NotNull Inv<E> fmap(@NotNull Function<E, E> f) {return new Inv<>(f.apply(i));}
+    public <T> @NotNull Inv<T> fmap(@NotNull Function<E, T> f) {return new Inv<>(f.apply(i));}
   }
   record Lit<E>(boolean isLeft) implements Formula<E> {
-    public @NotNull Lit<E> fmap(@NotNull Function<E, E> f) {return this;}
+    public <T> @NotNull Lit<T> fmap(@NotNull Function<E, T> f) {return new Lit<>(isLeft);}
   }
 }
